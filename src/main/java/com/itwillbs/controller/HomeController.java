2775de2d0c1d06710fe4d49c630179c18d02d4ba
YYJ <yunyj99@naver.com>
2022-07-05 15:39:16 +0900

    해시 암호화 알고리즘 (SHA-256)

--- src/main/java/com/itwillbs/controller/HomeController.java
+++ src/main/java/com/itwillbs/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.itwillbs.controller;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,24 +18,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
+		myHashing("SHA-256", "itwill");
+
 		return "home";
 	}
-	
+
+	// 해시 알고리즘 구현(암호화 : sha-256)
+	public void myHashing(String hshAlgorithm, String data) {
+
+		try {
+			// 핵심 알고리즘
+			MessageDigest md = MessageDigest.getInstance(hshAlgorithm);
+
+			// 암호화데이터를 바이트 배열의 형태로 변경
+			byte[] byteData = data.getBytes();
+			md.update(byteData);
+
+			byte[] digest = md.digest();
+
+			String hashData = "";
+			// 암호화 데이터를 16진수 형태로 변경
+			for (int i = 0; i < digest.length; i++) {
+				hashData += Integer.toHexString(digest[i] & 0xFF).toUpperCase();
+			}
+
+			System.out.println("암호화 성공 : " + hashData);
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
